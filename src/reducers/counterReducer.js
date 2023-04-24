bcf4aac4d82221534fb3035f9ae98fe47ff400cc
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-04-24 17:19:31 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,12 @@
 const counterReducer = (state,action) =>{
-
+switch(action.type){
+  case "Inc":
+    return state + 1
+  case "Dec":
+    return state - 1
+  default:
+    return state
+}
 }
 
 export {counterReducer}
